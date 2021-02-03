9d64e944b3d707410bb0cade36b24c800d3d47c3
Liam Sharp <lms464@scarletmail.rutgers.edu>
2021-02-03 17:34:16 -0500

    build works in test case

--- Scripts/Membrane_Binner.tcl
+++ Scripts/Membrane_Binner.tcl
@@ -4,30 +4,39 @@
 # Reference Membrane No Protein
 set PI 3.14159265359
 
+proc load_structure {inpt} {
+	 mol new "${inpt}"
+}
 
+load_structure "./membrane.pdb"
 set memb [atomselect 0 "resname DPPC"]
 set memb_minmax [measure minmax $memb]
 $memb delete
 
-set x [expr abs([lindex [lindex $memb_minmax 0]0]-[expr [lindex [lindex $memb_minmax 0]1])]
-set y [expr abs([lindex [lindex $memb_minmax 1]0]-[expr [lindex [lindex $memb_minmax 1]1])]
-set A [expr x * y]
+set r [vecsub [lindex $memb_minmax 1] [lindex $memb_minmax 0]]
+set x [lindex $r 0]
+set y [lindex $r 1]
+set A [expr $x * $y]
 
 
 set memb [atomselect 0 "resname DPPC and name P"]
 set numLip [$memb num]
 $memb delete
-set A_lip [expr 1.0*$A / (0.5*$numLip)]
+set A_lip [expr (0.5*$numLip)/(1.0*$A) ]
 
 
 # Membrane with protein embeded
 # dependant on which protein has been embedded
 
+load_structure "protein_mem.pdb"
+
 set memb [atomselect 1 "resname DPPC and name P"]
 set numLip_adj [$memb num]
 $memb delete
 
-set A_pro [expr 0.5*$numLip_adj / $A_lip]
+set A_adj [expr 0.5*$numLip_adj / $A_lip]
+set A_pro [expr $A- $A_adj]
 set A_bin [expr $A_pro / (2*$PI)]
 set bin_size [expr sqrt($A_bin)]
-set num_bin [expt 1.0*$A/$A_bin]
+set num_bin [expr int(1.0*$A/$A_bin)]
+
