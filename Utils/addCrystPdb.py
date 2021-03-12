9e127f7167c372cf515fdd36df14af5c98e6935c
Liam Sharp <lms464@scarletmail.rutgers.edu>
2021-03-12 11:21:35 -0500

    updated to read input.config.dat and strip pbc

--- Utils/addCrystPdb.py
+++ Utils/addCrystPdb.py
@@ -12,9 +12,27 @@ Last update: October 5, 2020
 from __future__ import print_function
 import argparse 
 
-def addCryst(pdbfile, boxinfo):
+def get_pbc(inpt):
+    # Added by Liam 3/12/2021
+    fl = open(inpt,'r')
+    line = fl.read()
+    bad_char = ["[", "]", "," ,'"']
+    for bc in bad_char:
+        line = line.replace(bc, " ")
+    line_split = line.split(':')[1].split(" ")#[1:-2]
+    pbc = []
+    for ls in line_split:
+        try:
+            pbc.append(float(ls))
+        except:
+            continue
+    return tuple(pbc)
+
+def addCryst(pdbfile, pbc): #boxinfo):
     inp_pdb = open(pdbfile, 'r').readlines()
+    #out_pdb = open(outpath+'/'+pdbfile, 'w')
     out_pdb = open(pdbfile, 'w')
+    boxinfo = get_pbc(pbc)
     wrtcryst = False
     for line in inp_pdb:
         if line.startswith('ATOM') and not wrtcryst:
@@ -32,8 +50,9 @@ def addCryst(pdbfile, boxinfo):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i',     dest='pdbfile', required=True, help='Input PDB File')
-    parser.add_argument('-cryst', dest='boxinfo', required=True, help='Crystal Information', nargs=6)
+    #parser.add_argument('-cryst', dest='boxinfo', required=True, help='Crystal Information', nargs=6)
+    parser.add_argument('-cryst',    dest='pbc', required=True, help='Path for output files')
     args = parser.parse_args()
 
-    addCryst(args.pdbfile, tuple(map(float, args.boxinfo)))
+    addCryst(args.pdbfile, args.pbc)#tuple(map(float, args.boxinfo))) #, args.outpath)
 
