583c5be98ada57fee0f05bde6af0f9b9322cb53e
Liam Sharp <lms464@scarletmail.rutgers.edu>
2021-03-31 23:13:02 -0400

    init

---
+++ Utils/parse.py
@@ -0,0 +1,45 @@
+fl = open('protein_mem00.pdb','r')
+lines = fl.readlines()
+
+res = []
+res_name = 0
+res_name_old = 0
+
+for li, line in enumerate(lines[1:]):
+    res_name = line[17:21]
+    if res_name == "ZMA ":
+        continue
+    if li != 0:
+        if res_name != res_name_old:
+            print("%s \t %i"%(res_name_old,len(res)))
+            res = []
+
+    if res_name == 'CHL1':
+        if line[13:15] != 'C3':
+            res_name_old = line[17:21]
+            continue
+        else:
+            res.append(res_name)
+            
+    if res_name == 'TIP3':
+        if line[13:16] == 'OH2':
+            res.append(res_name)
+            
+    if res_name == 'CLA ':
+        if line[13:16] == "CLA":
+            res.append(res_name) 
+    
+    if res_name == 'SOD ':
+        if line[13:16] == "SOD":
+            res.append(res_name) 
+    
+    if line[13:16] != 'P  ':
+        res_name_old = line[17:21]
+        continue
+    else:
+        res.append(res_name)
+        
+
+    res_name_old = line[17:21]
+    
+    
